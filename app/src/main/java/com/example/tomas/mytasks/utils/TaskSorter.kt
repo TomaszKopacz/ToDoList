5464be4a88081500dbbc01a12c83d6fc767a0f72
TomaszKopacz <tomasz.kopacz@onet.pl>
2018-10-22 17:21:22 +0200

    Sorting ready

--- app/src/main/java/com/example/tomas/mytasks/utils/TaskSorter.kt
+++ app/src/main/java/com/example/tomas/mytasks/utils/TaskSorter.kt
@@ -1,6 +1,8 @@
 package com.example.tomas.mytasks.utils
 
 import com.example.tomas.mytasks.entity.Task
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class TaskSorter {
@@ -48,33 +50,126 @@ class TaskSorter {
         return tasksToSort
     }
 
-    private fun quickSort(tasks: Array<Task>, x: Int, y: Int) {
+    private var leftPointer: Int = 0
+    private var rightPointer: Int = 0
+    private var thresholdTask: Task? = null
 
-        var i: Int = x
-        var j: Int = y
-        var temp: Task
-        val v: Task = tasks[(x + y) / 2]
+    private fun quickSort(tasks: Array<Task>, begin: Int, end: Int) {
+
+        leftPointer = begin
+        rightPointer = end
+        thresholdTask = tasks[(begin + end) / 2]
 
         do {
-            while (comparePriorities(tasks[i], v))
-                i++
-            while (comparePriorities(v, tasks[j]))
-                j--
-            if (i <= j) {
-                temp = tasks[i]
-                tasks[i] = tasks[j]
-                tasks[j] = temp
-                i++
-                j--
-            }
-        } while (i <= j)
-        if (x < j)
-            quickSort(tasks, x, j)
-        if (i < y)
-            quickSort(tasks, i, y)
+            movePointersUntilExchangeIsNeeded(tasks, begin, end)
+            exchangeTasks(tasks, leftPointer, rightPointer)
+            leftPointer++
+            rightPointer--
+
+        } while (leftPointer <= rightPointer)
+
+        if (begin < rightPointer)
+            quickSort(tasks, begin, rightPointer)
+        if (leftPointer < end)
+            quickSort(tasks, leftPointer, end)
     }
 
-    private fun comparePriorities(task1: Task, task2: Task): Boolean {
-        return task1.priority!! > task2.priority!!
+    private fun movePointersUntilExchangeIsNeeded(tasks: Array<Task>, begin: Int, end: Int){
+        leftPointer = begin
+        rightPointer = end
+
+        val middle = (begin + end) / 2
+        thresholdTask = tasks[middle]
+
+        while (isSecondTaskMoreImportant(tasks[leftPointer], thresholdTask!!) && leftPointer < middle)
+            leftPointer++
+        while (isSecondTaskMoreImportant(thresholdTask!!, tasks[rightPointer]) && rightPointer > middle)
+            rightPointer--
+    }
+
+    private fun exchangeTasks(tasks: Array<Task>, leftPointer: Int, rightPointer: Int) {
+        val temp = tasks[leftPointer]
+        tasks[leftPointer] = tasks[rightPointer]
+        tasks[rightPointer] = temp
+    }
+
+    private fun isSecondTaskMoreImportant(task1: Task, task2: Task): Boolean {
+        return isSecondTaskMoreImportantByPrimaryFields(task1, task2)
+    }
+
+    private fun isSecondTaskMoreImportantByPrimaryFields(task1: Task, task2: Task): Boolean {
+        return if (primaryField == null)
+            isSecondTaskMoreImportantBySecondlFields(task1, task2)
+        else {
+            val result = compareFields(task1, task2, primaryField!!)
+            when (result) {
+                FIELD_ONE_IS_GREATER -> false
+                FIELDS_ARE_THE_SAME -> isSecondTaskMoreImportantBySecondlFields(task1, task2)
+                else -> true
+            }
+        }
+    }
+
+    private fun isSecondTaskMoreImportantBySecondlFields(task1: Task, task2: Task): Boolean {
+        return if (secondaryField == null)
+            isSecondTaskMoreImportantByThirdFields(task1, task2)
+        else {
+            val result = compareFields(task1, task2, secondaryField!!)
+            when (result) {
+                FIELD_ONE_IS_GREATER -> false
+                FIELDS_ARE_THE_SAME -> isSecondTaskMoreImportantByThirdFields(task1, task2)
+                else -> true
+            }
+        }
+    }
+
+    private fun isSecondTaskMoreImportantByThirdFields(task1: Task, task2: Task): Boolean {
+        return if (thirdField == null)
+            return true
+        else {
+            val result = compareFields(task1, task2, thirdField!!)
+            when (result) {
+                FIELD_ONE_IS_GREATER -> false
+                else -> true
+            }
+        }
+    }
+
+    private fun compareFields(task1: Task, task2: Task, field: String): Int {
+        return when (field) {
+            PRIORITY -> comparePriorities(task1, task2)
+            DEADLINE -> compareDeadlines(task1, task2)
+            else -> compareCreationDates(task1, task2)
+        }
+    }
+
+    private fun comparePriorities(task1: Task, task2: Task): Int {
+        return when {
+            task1.priority!! > task2.priority!! -> FIELD_ONE_IS_GREATER
+            task1.priority!! < task2.priority!! -> FIELD_TWO_IS_GREATER
+            else -> FIELDS_ARE_THE_SAME
+        }
+    }
+
+    private fun compareDeadlines(task1: Task, task2: Task): Int {
+        val deadline1 = SimpleDateFormat(DatePatterns.DD_MM_YYYY_DASHES, Locale.getDefault()).parse(task1.deadline)
+        val deadline2 = SimpleDateFormat(DatePatterns.DD_MM_YYYY_DASHES, Locale.getDefault()).parse(task2.deadline)
+
+        return when {
+            deadline1 < deadline2 -> FIELD_ONE_IS_GREATER
+            deadline1 > deadline2 -> FIELD_TWO_IS_GREATER
+            else -> FIELDS_ARE_THE_SAME
+        }
+    }
+
+    private fun compareCreationDates(task1: Task, task2: Task): Int {
+        val date1 = SimpleDateFormat(DatePatterns.DD_MM_YYYY_DASHES, Locale.getDefault()).parse(task1.creationDate)
+        val date2 = SimpleDateFormat(DatePatterns.DD_MM_YYYY_DASHES, Locale.getDefault()).parse(task2.creationDate)
+
+        return when {
+            date1 < date2 -> FIELD_ONE_IS_GREATER
+            date1 > date2 -> FIELD_TWO_IS_GREATER
+            else -> FIELDS_ARE_THE_SAME
+        }
     }
 }
--- app/src/test/java/com/example/tomas/mytasks/utils/TaskSorterTest.kt
+++ app/src/test/java/com/example/tomas/mytasks/utils/TaskSorterTest.kt
@@ -7,7 +7,6 @@ import org.junit.Test
 
 class TaskSorterTest {
 
-    private lateinit var tasksToSort: ArrayList<Task>
     private lateinit var task1: Task
     private lateinit var task2: Task
     private lateinit var task3: Task
@@ -15,8 +14,6 @@ class TaskSorterTest {
 
     @Before
     fun init() {
-        tasksToSort = ArrayList()
-
         task1 = Task()
         task2 = Task()
         task3 = Task()
@@ -24,195 +21,144 @@ class TaskSorterTest {
 
         task1.creationDate = "01-01-2010"
         task1.deadline = "31-01-2010"
-        task1.priority = 1
+        task1.priority = 8
 
         task2.creationDate = "01-01-2010"
-        task2.deadline = "31-12-2020"
-        task2.priority = 2
+        task2.deadline = "31-12-2025"
+        task2.priority = 4
 
         task3.creationDate = "02-01-2010"
-        task3.deadline = "31-12-2020"
-        task3.priority = 3
+        task3.deadline = "31-12-2025"
+        task3.priority = 1
 
         task4.creationDate = "03-01-2010"
         task4.deadline = "01-06-2015"
-        task4.priority = 4
-
-        tasksToSort.add(task1)
-        tasksToSort.add(task2)
-        tasksToSort.add(task3)
-        tasksToSort.add(task4)
+        task4.priority = 1
     }
 
     @Test
-    fun testQuickSort() {
-        val arr = arrayOf(task1, task2, task3, task4)
-        val correct = arrayOf(task4, task3, task2, task1)
+    fun checkSortWhenOneFieldIsSetAtPrimary(){
+        val testArray = arrayOf(task1, task2, task3, task4)
+        val correctArray = arrayOf(task4, task3, task2, task1)
 
         val sorter = TaskSorter()
-        val sortedArr = sorter.sort(arr)
+        sorter.setPrimaryField(TaskSorter.PRIORITY)
 
-        assertEquals(correct[0].priority, sortedArr[0].priority)
-        assertEquals(correct[1].priority, sortedArr[1].priority)
-        assertEquals(correct[2].priority, sortedArr[2].priority)
-        assertEquals(correct[3].priority, sortedArr[3].priority)
+        val sortedArray = sorter.sort(testArray)
+        assertEquals(correctArray, sortedArray)
+        assertEquals(correctArray[0].priority, sortedArray[0].priority)
+        assertEquals(correctArray[1].priority, sortedArray[1].priority)
+        assertEquals(correctArray[2].priority, sortedArray[2].priority)
+        assertEquals(correctArray[3].priority, sortedArray[3].priority)
     }
 
-//    @Test
-//    fun checkPriorityAsPrimary(){
-//        val sorter = TaskSorter()
-//        sorter.setPrimaryField(TaskSorter.PRIORITY)
-//
-//        val correctList = ArrayList<Task>()
-//        correctList.add(task4)
-//        correctList.add(task3)
-//        correctList.add(task2)
-//        correctList.add(task1)
-//
-//        val sortedTasks = sorter.sort(tasksToSort)
-//        assertEquals(correctList, sortedTasks)
-//    }
-//
-//    @Test
-//    fun checkSortWhenOneFieldIsSetAtPrimary(){
-//        val sorter = TaskSorter()
-//        sorter.setPrimaryField(TaskSorter.PRIORITY)
-//
-//        val correctList = ArrayList<Task>()
-//        correctList.add(task2)
-//        correctList.add(task4)
-//        correctList.add(task3)
-//        correctList.add(task1)
-//
-//        assertEquals(4, tasksToSort.size)
-//
-//        val sortedTasks = sorter.sort(tasksToSort)
-//        assertEquals(correctList, sortedTasks)
-//    }
-//
-//    @Test
-//    fun checkSortWhenOneFieldIsSetAtSecondary(){
-//        val sorter = TaskSorter()
-//        sorter.setSecondaryField(TaskSorter.PRIORITY)
-//
-//        val correctList = ArrayList<Task>()
-//        correctList.add(task2)
-//        correctList.add(task4)
-//        correctList.add(task3)
-//        correctList.add(task1)
-//
-//        val sortedTasks = sorter.sort(tasksToSort)
-//        assertEquals(correctList, sortedTasks)
-//    }
-//
-//    @Test
-//    fun checkSortWhenOneFieldIsSetAtThird(){
-//        val sorter = TaskSorter()
-//        sorter.setThirdField(TaskSorter.PRIORITY)
-//
-//        val correctList = ArrayList<Task>()
-//        correctList.add(task2)
-//        correctList.add(task4)
-//        correctList.add(task3)
-//        correctList.add(task1)
-//
-//        val sortedTasks = sorter.sort(tasksToSort)
-//        assertEquals(correctList, sortedTasks)
-//    }
-//
-//    @Test
-//    fun checkSortWhenTwoFieldsAreSetAtPrimaryAndSecondary(){
-//        val sorter = TaskSorter()
-//        sorter.setPrimaryField(TaskSorter.PRIORITY)
-//        sorter.setSecondaryField(TaskSorter.DEADLINE)
-//
-//        val correctList = ArrayList<Task>()
-//        correctList.add(task2)
-//        correctList.add(task4)
-//        correctList.add(task3)
-//        correctList.add(task1)
-//
-//        val sortedTasks = sorter.sort(tasksToSort)
-//        assertEquals(correctList, sortedTasks)
-//    }
-//
-//    @Test
-//    fun checkSortWhenTwoFieldsAreSetAtPrimaryAndThird(){
-//        val sorter = TaskSorter()
-//        sorter.setPrimaryField(TaskSorter.DEADLINE)
-//        sorter.setThirdField(TaskSorter.PRIORITY)
-//
-//        val correctList = ArrayList<Task>()
-//        correctList.add(task1)
-//        correctList.add(task4)
-//        correctList.add(task2)
-//        correctList.add(task3)
-//
-//        val sortedTasks = sorter.sort(tasksToSort)
-//        assertEquals(correctList, sortedTasks)
-//    }
-//
-//    @Test
-//    fun checkSortWhenTwoFieldsAreSetAtSecondaryAndThird(){
-//        val sorter = TaskSorter()
-//        sorter.setSecondaryField(TaskSorter.CREATION_DATE)
-//        sorter.setThirdField(TaskSorter.PRIORITY)
-//
-//        val correctList = ArrayList<Task>()
-//        correctList.add(task2)
-//        correctList.add(task1)
-//        correctList.add(task3)
-//        correctList.add(task4)
-//
-//        val sortedTasks = sorter.sort(tasksToSort)
-//        assertEquals(correctList, sortedTasks)
-//    }
-//
-//    @Test
-//    fun checkSortWhenThreeFieldsAreSet(){
-//        val sorter = TaskSorter()
-//        sorter.setPrimaryField(TaskSorter.DEADLINE)
-//        sorter.setSecondaryField(TaskSorter.CREATION_DATE)
-//        sorter.setThirdField(TaskSorter.PRIORITY)
-//
-//        val correctList = ArrayList<Task>()
-//        correctList.add(task1)
-//        correctList.add(task4)
-//        correctList.add(task2)
-//        correctList.add(task3)
-//
-//        val sortedTasks = sorter.sort(tasksToSort)
-//        assertEquals(correctList, sortedTasks)
-//    }
-//
-//    @Test
-//    fun checkSortWhenThreeFieldsAreSetAtSamePosition(){
-//        val sorter = TaskSorter()
-//        sorter.setSecondaryField(TaskSorter.DEADLINE)
-//        sorter.setSecondaryField(TaskSorter.CREATION_DATE)
-//        sorter.setSecondaryField(TaskSorter.PRIORITY)
-//
-//        val correctList = ArrayList<Task>()
-//        correctList.add(task2)
-//        correctList.add(task3)
-//        correctList.add(task4)
-//        correctList.add(task1)
-//
-//        val sortedTasks = sorter.sort(tasksToSort)
-//        assertEquals(correctList, sortedTasks)
-//    }
-//
-//    @Test
-//    fun checkSortWhenNoneOfFieldsIsSet(){
-//        val sorter = TaskSorter()
-//
-//        val correctList = ArrayList<Task>()
-//        correctList.add(task1)
-//        correctList.add(task2)
-//        correctList.add(task3)
-//        correctList.add(task4)
-//
-//        val sortedTasks = sorter.sort(tasksToSort)
-//        assertEquals(correctList, sortedTasks)
-//    }
+    @Test
+    fun checkSortWhenOneFieldIsSetAtSecondary(){
+        val testArray = arrayOf(task1, task2, task3, task4)
+        val correctArray = arrayOf(task4, task3, task2, task1)
+
+        val sorter = TaskSorter()
+        sorter.setSecondaryField(TaskSorter.PRIORITY)
+
+        val sortedArray = sorter.sort(testArray)
+        assertEquals(correctArray, sortedArray)
+    }
+
+    @Test
+    fun checkSortWhenOneFieldIsSetAtThird(){
+        val testArray = arrayOf(task1, task2, task3, task4)
+        val correctArray = arrayOf(task4, task3, task2, task1)
+
+        val sorter = TaskSorter()
+        sorter.setThirdField(TaskSorter.PRIORITY)
+
+        val sortedArray = sorter.sort(testArray)
+        assertEquals(correctArray, sortedArray)
+    }
+
+    @Test
+    fun checkSortWhenTwoFieldsAreSetAtPrimaryAndSecondary(){
+        val testArray = arrayOf(task1, task2, task3, task4)
+        val correctArray = arrayOf(task3, task4, task2, task1)
+
+        val sorter = TaskSorter()
+        sorter.setPrimaryField(TaskSorter.PRIORITY)
+        sorter.setSecondaryField(TaskSorter.DEADLINE)
+
+        val sortedArray = sorter.sort(testArray)
+        assertEquals(correctArray, sortedArray)
+    }
+
+    @Test
+    fun checkSortWhenTwoFieldsAreSetAtPrimaryAndThird(){
+        val testArray = arrayOf(task1, task2, task3, task4)
+        val correctArray = arrayOf(task3, task2, task4, task1)
+
+        val sorter = TaskSorter()
+        sorter.setPrimaryField(TaskSorter.DEADLINE)
+        sorter.setThirdField(TaskSorter.PRIORITY)
+
+        val sortedArray = sorter.sort(testArray)
+        assertEquals(correctArray, sortedArray)
+        assertEquals(correctArray[0].priority, sortedArray[0].priority)
+        assertEquals(correctArray[1].priority, sortedArray[1].priority)
+        assertEquals(correctArray[2].priority, sortedArray[2].priority)
+        assertEquals(correctArray[3].priority, sortedArray[3].priority)
+    }
+
+    @Test
+    fun checkSortWhenTwoFieldsAreSetAtSecondaryAndThird(){
+        val testArray = arrayOf(task1, task2, task3, task4)
+        val correctArray = arrayOf(task4, task3, task2, task1)
+
+        val sorter = TaskSorter()
+        sorter.setSecondaryField(TaskSorter.CREATION_DATE)
+        sorter.setThirdField(TaskSorter.PRIORITY)
+
+        val sortedArray = sorter.sort(testArray)
+        assertEquals(correctArray, sortedArray)
+    }
+
+    @Test
+    fun checkSortWhenThreeFieldsAreSet(){
+        val testArray = arrayOf(task1, task2, task3, task4)
+        val correctArray = arrayOf(task3, task2, task4, task1)
+
+        val sorter = TaskSorter()
+        sorter.setPrimaryField(TaskSorter.DEADLINE)
+        sorter.setSecondaryField(TaskSorter.CREATION_DATE)
+        sorter.setThirdField(TaskSorter.PRIORITY)
+
+        val sortedArray = sorter.sort(testArray)
+        assertEquals(correctArray, sortedArray)
+        assertEquals(correctArray[0].priority, sortedArray[0].priority)
+        assertEquals(correctArray[1].priority, sortedArray[1].priority)
+        assertEquals(correctArray[2].priority, sortedArray[2].priority)
+        assertEquals(correctArray[3].priority, sortedArray[3].priority)
+    }
+
+    @Test
+    fun checkSortWhenThreeFieldsAreSetAtSamePosition(){
+        val testArray = arrayOf(task1, task2, task3, task4)
+        val correctArray = arrayOf(task4, task3, task2, task1)
+
+        val sorter = TaskSorter()
+        sorter.setSecondaryField(TaskSorter.DEADLINE)
+        sorter.setSecondaryField(TaskSorter.CREATION_DATE)
+        sorter.setSecondaryField(TaskSorter.PRIORITY)
+
+        val sortedArray = sorter.sort(testArray)
+        assertEquals(correctArray, sortedArray)
+    }
+
+    @Test
+    fun checkSortWhenNoneOfFieldsIsSet(){
+        val testArray = arrayOf(task1, task2, task3, task4)
+        val correctArray = arrayOf(task1, task2, task3, task4)
+
+        val sorter = TaskSorter()
+
+        val sortedArray = sorter.sort(testArray)
+        assertEquals(correctArray, sortedArray)
+    }
 }
